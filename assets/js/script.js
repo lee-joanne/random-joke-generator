8eb4fa91468064100f35052fd62397293e9de6b5
Joanne <lee.joanne54@gmail.com>
2023-10-17 05:48:11 +0000

    Add API and wire correctly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("Connected!")
+let jokeText = document.getElementById("joke-text");
+let btn = document.getElementById("joke-btn");
+
+btn.addEventListener("click", async () => {
+    try {
+        const response = await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single");
+        const responseText = await response.text();
+        const jokeData = JSON.parse(responseText);
+        const joke = jokeData.joke
+        jokeText.innerHTML = joke;
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+});
